ea0a3b3a00442b46ca956cd8ff2616998bbf7838
petra <Petra Barus>
2017-08-04 21:57:51 +0700

    Output diff for every commit.

--- src/main/java/net/petrabarus/periodic_git_sync/App.java
+++ src/main/java/net/petrabarus/periodic_git_sync/App.java
@@ -1,16 +1,23 @@
 package net.petrabarus.periodic_git_sync;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.revwalk.RevCommit;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.revwalk.DepthWalk.RevWalk;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 
 public class App {
 
@@ -42,9 +49,9 @@ public class App {
             String name = UUID.randomUUID().toString();
             File file = directory.resolve(name).toFile();
             file.createNewFile();
-            FileWriter writer = new FileWriter(file);
-            writer.write(name);
-            writer.close();
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write("Content: " + name);
+            }
             return name;
         } catch (IOException ex) {
             Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
@@ -54,9 +61,35 @@ public class App {
 
     public static void printCommits(Git git) throws IOException, GitAPIException {
         Iterable<RevCommit> commits1 = git.log().all().call();
-        System.out.println("Current commit");
-        for (RevCommit commit : commits1) {
-            System.out.println("\t" + commit.getName()); //The hash
+        Iterator<RevCommit> iterator = commits1.iterator();
+
+        RevCommit next = iterator.next();
+        System.out.println(next.getName());
+        while (iterator.hasNext()) {
+            RevCommit current = iterator.next();
+            System.out.println(current.getName());
+            printDiff(git, current, next);
+            next = current;
         }
+    }
+
+    public static CanonicalTreeParser getTreeParser(Git git, RevCommit commit) throws IOException {
+        try (RevWalk walk = new RevWalk(git.getRepository(), 0);
+                ObjectReader reader = git.getRepository().newObjectReader()) {
+            return new CanonicalTreeParser(null, reader, commit.getTree());
+        }
+    }
+
+    public static void printDiff(Git git, RevCommit prev, RevCommit next) throws IOException, GitAPIException {
+        System.out.println("-------------------------------------------------------------------------------------------");
+        CanonicalTreeParser oldTreeIterator = getTreeParser(git, prev);
+        CanonicalTreeParser newTreeIterator = getTreeParser(git, next);
+        try (OutputStream outputStream = new ByteArrayOutputStream();
+                DiffFormatter formatter = new DiffFormatter(outputStream)) {
+            formatter.setRepository(git.getRepository());
+            formatter.format(oldTreeIterator, newTreeIterator);
+            System.out.println(outputStream.toString());
+        }
+        System.out.println("===========================================================================================");
     }
 }
