14cbb34ddb233c3d2e543708915cf36b48f797c0
petra <Petra Barus>
2017-08-05 00:37:41 +0700

    Add periodic commit and append.

--- src/main/java/net/petrabarus/periodic_git_sync/App.java
+++ src/main/java/net/petrabarus/periodic_git_sync/App.java
@@ -1,5 +1,8 @@
 package net.petrabarus.periodic_git_sync;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
@@ -7,34 +10,111 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.revwalk.RevCommit;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.RevisionSyntaxException;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.revwalk.DepthWalk.RevWalk;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 
 public class App {
 
-    public static void main(String[] argv) throws IOException, GitAPIException {
-        Path path = Paths.get(argv[0]);
-        Git git = loadOrCreateRepo(path);
+    @Parameter(names = "--directory")
+    public String directory;
 
-        String name = createRandomFile(path);
-        if (name == null) {
-            return;
+    @Parameter(names = "--log")
+    public String log;
+
+    @Parameter(names = "--duration")
+    public Integer duration = 60000;
+
+    private Git git;
+
+    private File logFile;
+
+    public static class PeriodicCommitTask implements Runnable {
+
+        private final Git git;
+
+        private final File logFile;
+
+        public PeriodicCommitTask(Git git, File logFile) {
+            this.git = git;
+            this.logFile = logFile;
         }
 
-        git.add().addFilepattern(name).call();
+        @Override
+        public void run() {
+            System.out.println("Committing files");
+            try {
+                git.add().addFilepattern(".").call();
+                String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+                git.commit().setMessage("Commit " + timestamp).call();
 
-        git.commit().setMessage("Add file: " + name).call();
+                Iterable<RevCommit> commits = git.log().setMaxCount(2).all().call();
+                Iterator<RevCommit> iterator = commits.iterator();
+                RevCommit second = iterator.next();
+                RevCommit first = iterator.next();
+                String diffText = getDiffText(git, first, second);
+                writeDiffText(diffText);
 
-        printCommits(git);
+            } catch (GitAPIException ex) {
+                //
+            } catch (IncorrectObjectTypeException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (RevisionSyntaxException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        private void writeDiffText(String diffText) {
+            try (FileWriter fileWriter = new FileWriter(logFile, true);
+                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+                bufferedWriter.write(diffText);
+                bufferedWriter.write("=========================================\n");
+            } catch (IOException ex) {
+
+            }
+        }
+    }
+
+    public static void main(String[] argv) throws IOException, GitAPIException {
+        App app = new App();
+        JCommander.newBuilder()
+                .addObject(app)
+                .build()
+                .parse(argv);
+        app.run();
+    }
+
+    public void run() throws GitAPIException, IOException {
+        git = loadOrCreateRepo(Paths.get(directory));
+        logFile = Paths.get(log).toFile();
+        createFileIfNotExists();
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        PeriodicCommitTask task = new PeriodicCommitTask(git, logFile);
+        scheduler.scheduleAtFixedRate(task, 0, duration, TimeUnit.SECONDS);
+    }
+
+    private void createFileIfNotExists() throws IOException {
+        if (!logFile.exists()) {
+            logFile.createNewFile();
+        }
     }
 
     public static Git loadOrCreateRepo(Path dirPath) throws GitAPIException {
@@ -59,37 +139,19 @@ public class App {
         }
     }
 
-    public static void printCommits(Git git) throws IOException, GitAPIException {
-        Iterable<RevCommit> commits1 = git.log().all().call();
-        Iterator<RevCommit> iterator = commits1.iterator();
-
-        RevCommit next = iterator.next();
-        System.out.println(next.getName());
-        while (iterator.hasNext()) {
-            RevCommit current = iterator.next();
-            System.out.println(current.getName());
-            printDiff(git, current, next);
-            next = current;
-        }
-    }
-
     public static CanonicalTreeParser getTreeParser(Git git, RevCommit commit) throws IOException {
-        try (RevWalk walk = new RevWalk(git.getRepository(), 0);
-                ObjectReader reader = git.getRepository().newObjectReader()) {
-            return new CanonicalTreeParser(null, reader, commit.getTree());
-        }
+        ObjectReader reader = git.getRepository().newObjectReader();
+        return new CanonicalTreeParser(null, reader, commit.getTree());
     }
 
-    public static void printDiff(Git git, RevCommit prev, RevCommit next) throws IOException, GitAPIException {
-        System.out.println("-------------------------------------------------------------------------------------------");
+    public static String getDiffText(Git git, RevCommit prev, RevCommit next) throws IOException, GitAPIException {
         CanonicalTreeParser oldTreeIterator = getTreeParser(git, prev);
         CanonicalTreeParser newTreeIterator = getTreeParser(git, next);
         try (OutputStream outputStream = new ByteArrayOutputStream();
                 DiffFormatter formatter = new DiffFormatter(outputStream)) {
             formatter.setRepository(git.getRepository());
             formatter.format(oldTreeIterator, newTreeIterator);
-            System.out.println(outputStream.toString());
+            return outputStream.toString();
         }
-        System.out.println("===========================================================================================");
     }
 }
